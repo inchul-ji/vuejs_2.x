9826c5e8fe558a97d7614e1964ad12e0910a49a1
inchul-ji <69510154+inchul-ji@users.noreply.github.com>
2023-01-16 09:35:41 +0900

    test

--- intermediate/vue-todo/src/main.js
+++ intermediate/vue-todo/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import { store } from '@/store/store';
+
 Vue.config.productionTip = false;
+
+console.log('test');
 
 new Vue({
   render: h => h(App),
